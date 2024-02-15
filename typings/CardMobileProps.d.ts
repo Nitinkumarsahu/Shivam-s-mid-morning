c41b57328f305b2e7b2b03d9637c9f9788477d7f
Nitin KumarSahu <83062954+Nitinkumarsahu@users.noreply.github.com>
2024-02-15 23:14:33 +0530

    Props

---
+++ typings/CardMobileProps.d.ts
@@ -0,0 +1,13 @@
+type CardProps = {
+  backgroundStyle: string;
+  title: string;
+  index: number;
+  setActiveCard: Function;
+  currCard: number;
+  theme: any;
+  description: string;
+  imageLink: string;
+  projectLink: string;
+};
+
+export default CardProps;
---
+++ typings/CardProps.d.ts
@@ -0,0 +1,11 @@
+type CardProps = {
+  backgroundStyle: any;
+  title: string;
+  index: number;
+  theme: any;
+  description: string;
+  imageLink: string;
+  projectLink: string;
+};
+
+export default CardProps;
