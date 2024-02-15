9c33abcac0f852f4d179c1a395e0cd4c2f3c57f0
Nitin KumarSahu <83062954+Nitinkumarsahu@users.noreply.github.com>
2024-02-15 23:09:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        morningblue: "#84CAE7",
+        morningyellow: "#FFD000",
+        colorBehance: "#1769FF",
+        colorTwitter: "#00ACEE",
+        colorGithub: "#333333",
+        colorInsta: "#D62976",
+      },
+      spacing: {
+        "47%": "47%",
+        64: "64rem",
+      },
+      animation: {
+        peekRight: "1s ease-in alternate-reverse peekright infinite",
+        peekBottom: "1s ease-in alternate-reverse peekbottom infinite",
+      },
+      keyframes: {
+        peekright: {
+          "0%, 100%": { left: "0.25rem" },
+          "50%": { left: "1.25rem" },
+        },
+        peekbottom: {
+          "0%, 100%": { top: "0.25rem" },
+          "100%": { top: "1.25rem" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
